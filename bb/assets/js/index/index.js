3f0a34012a431ae8e4a048bd808ffc85f379b56a
Leon <leon@LeondeMacBook-Pro.local>
2015-08-30 03:07:37 +0800

    fix bug

--- bb/assets/js/index/index.js
+++ bb/assets/js/index/index.js
@@ -45,29 +45,60 @@ var index = {
         $$('#index').on(clickEvent,'a.pipei', function () {
             var btn = $$(this);
             index.functions.check_follow(function () {
+                mainFramework.showPreloader("匹配中...");
                 btn.addClass('hide');
-                index.functions.pipei(function () {
+                index.functions.pipei(function (user) {
+                    mainFramework.hidePreloader();
                     $$('#index a.pipei-success').removeClass('hide');
+                    //将拿到的数据展示出来
+                    var sex_color = 'color-pink';
+                    if(user.tag.sex = 1){
+                        sex_color = 'color-blue';
+                    }
+                    var name = user.tag.nickname;
+                    if(name == null || name == '' || name == undefined){
+                        name = '未知';
+                    }
+                    var pic = user.tag.avatar;
+                    if(pic == null || pic == '' || pic == undefined){
+                        pic = '../addons/lonaking_bb/template/mobile/images/head.jpg';
+                    }
+                    var over_minute = user.over_minute;
+                    var over_minute_notice = "未知时间后结束";
+                    if(over_minute != null && over_minute != '' && over_minute != undefined){
+                        var over_minute_notice = '聊天将在'+over_minute+'分种后结束';
+                    }
+                    var current_chat_user = '<li><div class="item-content"><div class="item-media"><img src="'+pic+'" width="44"></div><div class="item-inner"><div class="item-title-row"><div class="item-title">'+name+' &nbsp;<i class="icon-user '+sex_color+'"></i></div><div class="item-after">'+over_minute_notice+'</div></div><div class="item-subtitle">'+user.tag.province +''+user.tag.city +'['+user.tag.country+']'+'</div></div></div></li>';
+                    $('div.current-chat-info ul').empty();
+
+                    $$('div.current-chat-info ul').append(current_chat_user);
                 }, function () {
+                    mainFramework.hidePreloader();
                     btn.removeClass('hide');
                 });
             });
+            $$('div.ripple-wave.ripple-wave-fill').remove();
         });
 
         /**
          * 挂断
          */
         $$('#index').on(clickEvent,'a.hang-up', function () {
+            mainFramework.showPreloader("正在挂断...");
             var btn = $$(this);
             index.functions.check_follow(function () {
                 btn.addClass('hide');
                 $$('#index a.load.hide').removeClass('hide');
                 index.functions.hang_up(function () {
+                    mainFramework.hidePreloader();
                     $$('#index a.load').addClass('hide');
                     $$('#index a.pipei').removeClass('hide');
                     $$('#index a.hang-up').addClass('hide');
+                    $('div.current-chat-info ul').empty();
+
                 });
             });
+            $$('div.ripple-wave.ripple-wave-fill').remove();
         });
 
     },
@@ -79,10 +110,9 @@ var index = {
                 $$('#index a.load').addClass('hide');
                 var json = JSON.parse(e);
                 if(json.status == 200){
-                    callback();
+                    mainFramework.alert(json.message,callback(json.data));
                 }else{
-                    mainFramework.alert(json.message);
-                    error_callback();
+                    mainFramework.alert(json.message,error_callback());
                 }
             });
         },
