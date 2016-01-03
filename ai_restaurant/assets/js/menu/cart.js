78463416a74b828881bdf966a56524f6eda6ccd1
lonaking <lonaking@foxmail.com>
2016-01-03 22:09:37 +0800

    完成前端

---
+++ ai_restaurant/assets/js/menu/cart.js
@@ -0,0 +1,124 @@
+var cart = {
+    cart : {
+        count : 0,//人数
+        menuCount : 0,//菜数
+        //菜品
+        food : {
+
+        },
+        totalAmount : 0.0,
+        aaAmount : 0.0,
+        onlyAmount : 0.0
+    },
+    cartPosition : {
+        top : 0,
+        left : 0,
+    },
+    init : function () {
+        //初始化 购物车位置
+        cart.initCartPosition();
+        cart.event();
+
+    },
+    event : function () {
+
+        //点击-
+        $(".list-item").on('click','.btn.-minus', function (e) {
+            e.stopPropagation();
+            var foodId = $(this).parents(".list-item").data("item");
+            //alert(foodId);
+
+        });
+
+        //点击+
+        $(".list-item").on('click','.btn.-plus', function (e) {
+            e.stopPropagation();
+            var foodId = $(this).parents(".list-item").data("item");
+            //alert(foodId);
+            var windowHeight = $(window).height();
+            //alert(windowHeight);
+            //alert(getY(e));
+            var offset = $(this).offset();
+            var startTop = offset.top;
+            var startLeft = offset.left;
+            //判断高度
+            cart.fly(startTop,startLeft, function () {
+                //shopCartAnimate
+            });
+        });
+
+        //点击购物车
+        $("#shopmenu-cart-bar").on('click','.row-num', function (e) {
+            e.stopPropagation();
+            var cartMenuButton = $("#shopmenu-cart-list");
+            var status = cartMenuButton.data("status");
+            if(status == "down"){
+                cartMenuButton.removeClass("visiHidden");
+                cartMenuButton.removeClass("down");
+                cartMenuButton.addClass("up");
+                cartMenuButton.data("status","up");
+                $(".global-mask").show();
+            }else if(status == "up"){
+                cartMenuButton.removeClass("up");
+                cartMenuButton.addClass("down");
+                cartMenuButton.data("status","down");
+                cartMenuButton.removeClass("visiHidden");
+                $(".global-mask").hide();
+            }
+        });
+        //关闭购物车
+        $(".global-mask").on("click", function (e) {
+            e.stopPropagation();
+            var cartMenuButton = $("#shopmenu-cart-list");
+            cartMenuButton.removeClass("up");
+            cartMenuButton.addClass("down");
+            cartMenuButton.data("status","down");
+            cartMenuButton.removeClass("visiHidden");
+            $(".global-mask").hide();
+        });
+    },
+
+    /*飞向购物车*/
+    fly : function (startTop, startLeft, success) {
+        var flyer = $('<em class="cart-count" style="display: block"></em>'); //抛物体对象
+        flyer.fly({
+            start : {
+                top: startTop,  //开始位置（必填）#fly元素会被设置成position: fixed
+                left: startLeft,  //开始位置（必填）
+            },
+            end : {
+                left: cart.cartPosition.left, //结束位置（必填）
+                top: cart.cartPosition.top,  //结束位置（必填）
+                width: 20, //结束时高度
+                height: 20, //结束时高度
+            },
+            speed : 1.8,
+            onEnd : function () {
+                $(".row-num").addClass("shopCartAnimate");
+                this.destroy();
+                setTimeout(function () {
+                    $(".row-num").removeClass("shopCartAnimate");
+                },200);
+
+            }
+        });
+
+    },
+
+    /*初始化购物车位置*/
+    initCartPosition : function () {
+        var offset = $("em.cart-count").offset();
+        cart.cartPosition.top = offset.top;
+        cart.cartPosition.left = offset.left;
+    },
+};
+function getX(e) {
+    e = e || window.event;
+
+    return e.pageX || e.clientX + document.body.scroolLeft;
+}
+
+function getY(e) {
+    e = e|| window.event;
+    return e.pageY || e.clientY + document.boyd.scrollTop;
+}
---
+++ ai_restaurant/assets/js/menu/menu.js
@@ -0,0 +1,48 @@
+var menu = {
+
+    menuGroupTopList : {
+
+    },
+    init : function(){
+        //初始化高度 菜单高度
+        $(".shopmenu-list ul.listgroup").each(function (index, item) {
+            var top = $(item).offset().top;
+            menu.menuGroupTopList['group'+index] = top;
+        });
+        //事件
+        menu.event();
+
+    },
+    event : function(){
+        //点击菜单
+        $("ul.list-group").on('click','.list-item', function (e) {
+            e.stopPropagation();
+            var btn = $(this);
+            $("ul.list-group .list-item.active").removeClass('active');
+            btn.addClass('active');
+            //显示本类菜品
+            var index = btn.data("index");
+            //var menuList = $(".shopmenu-list .listgroup[data-id="+index+"]");
+            //var currentMenuTop = menuList.offset().top;
+            var currentMenuTop = menu.menuGroupTopList['group'+index];
+            $('html,body').animate({scrollTop: currentMenuTop+'px'}, 300);
+        });
+        //滚动条滚动
+        $(window).scroll(function(e) {
+            e.stopPropagation();
+            $(".shopmenu-list ul.listgroup").each(function (index, item) {
+                //alert(index);
+                var groupId = $(item).data("id");
+                var groupTop = menu.menuGroupTopList['group'+index];
+                var top = $(window).scrollTop();
+                if(top == groupTop){
+                    //菜单栏改为active
+                    $("ul.list-group .active").removeClass('active');
+                    $("ul.list-group .list-item[data-index="+groupId+"]").addClass('active');
+                }
+            });
+        });
+        //$('.actGotop').click(function(){
+        //$('html,body').animate({scrollTop: '0px'}, 800);});
+    }
+}
