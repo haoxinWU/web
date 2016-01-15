f185b5cdcb540af6fd811400292d3733f41cca49
Leon <leon@LeondeMacBook-Pro-2.local>
2016-01-15 14:39:27 +0800

    tmp

--- ai_restaurant/assets/js/comment/comment.js
+++ ai_restaurant/assets/js/comment/comment.js
@@ -1,9 +1,54 @@
 var comment = {
+    orderId : null,
+    urls : {
+        commentApi : null,
+        shopCommentPage : null,
+    },
     init : function(){
 
+        comment.orderId = $("html").data("order-id");
+
+        comment.urls.commentApi = $("html").data("comment-api");
+        comment.urls.shopCommentPage = $("html").data("shop-comment-page");
+
+        comment.event();
     },
 
     event : function(){
+        $(".submit-comment").on("touchstart", function (e) {
+            e.stopPropagation();
+            var star = 0;//TODO
+            var content = $("textarea[name=comment]").val();
+            if(null == content || content == ""){
+                //alert("评价内容不能为空");
+                loading.tip("评价内容不能为空");
+                return false;
+            };
+            loading.load("提交中...");
+            comment.doComment(comment.orderId, content, function (json) {
+                if(json.status == 200){
+                    //TODO 显示提交成功 2秒后跳转
+                    setTimeout(function () {
+                        window.location.href = comment.urls.shopCommentPage;
+                    },2000);
+                }else{
+                    //TODO 提示
+                }
+            });
+        });
+    },
 
+    //评论
+    doComment : function(orderId, content, callback){
+        var url = comment.urls.commentApi;
+        var data = {
+            order_id : comment.orderId,
+            content : content
+        };
+
+        $.post(url, data, function (res) {
+            var json = JSON.parse(res);
+            callback(json);
+        });
     }
 };
--- ai_restaurant/assets/js/common/loading.js
+++ ai_restaurant/assets/js/common/loading.js
@@ -13,12 +13,28 @@ var loading = {
             loading.load(cusLoadingMessage);
         });
     },
-    
+
+
     load : function (message) {
         $("#loading-message").text(loading.defaultMessage);
         $("#page-loader").show();
     },
-    
+
+    //<div id="alert-tip" class="alert-tip" style="bottom: 80px; display: none;"></div>
+    tip : function (alertMessage) {
+        $("#alert-tip").text(alertMessage);
+        var left = ($(window).width()- $("#alert-tip").outerWidth()) / 2 + "px";
+        $("#alert-tip").css({
+            "bottom": "80px",
+            "left": left
+        });
+        $("#alert-tip").show();
+        setTimeout(function () {
+            $("#alert-tip").hide();
+            $("#alert-tip").text("");
+        },2000);
+    },
+
     hide : function () {
         $("#page-loader").hide();
     }
